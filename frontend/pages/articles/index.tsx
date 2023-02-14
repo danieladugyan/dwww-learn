527580014d2dccf39714d4d22ab5877133e63293
Daniel Adu-Gyan <danny_adu@hotmail.com>
2023-02-14 16:05:10 +0100

    Next.js - routing

---
+++ frontend/pages/articles/index.tsx
@@ -0,0 +1,15 @@
+import { Article } from "../../components/Article";
+
+const Articles = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Article />
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Articles;
